3c525b58b3e5fba844cfdaa997edb0b4a76776e7
Anastasiia Zahorodnia <anzahoro@c3a4c2.42berlin.de>
2025-01-21 19:35:27 +0100

    The projects in the C00 folder of the 42b-piscine repository involve foundational C programming exercises aimed at mastering basic concepts like variables, conditionals, loops, and functions.

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anzahoro <anzahoro@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 14:59:12 by anzahoro          #+#    #+#             */
+/*   Updated: 2024/10/02 18:17:15 by anzahoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+}
+/*
+int	main(void) {
+	ft_print_alphabet();
+	return 0;
+}
+*/
