de92d1fd849422b378f28b5db7be8d979deb7cc1
anzahoro <109831034+AnastasiaZahorodnia@users.noreply.github.com>
2025-01-21 19:58:17 +0100

    Add files via upload

---
+++ C00/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anzahoro <anzahoro@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 15:43:21 by anzahoro          #+#    #+#             */
+/*   Updated: 2024/10/02 18:21:41 by anzahoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+/*
+int	main(void) {
+    ft_print_reverse_alphabet();
+    return 0;
+}
+*/
